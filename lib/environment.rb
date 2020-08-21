0750657a30f621915126c6af60736250f7579976
kbritt510 <kbritt510@gmail.com>
2020-08-21 15:47:53 -0500

    Fixed everything git pull

---
+++ lib/environment.rb
@@ -0,0 +1,16 @@
+require 'bundler/setup'
+Bundler.require
+require 'net/http'
+require 'open-uri'
+require 'json'
+require 'pry'
+require_relative './get_weapons.rb'
+require_relative './weapon.rb'
+require_relative './main_menu.rb'
+
+# modules here
+
+# require_relative './MainMenu'
+# require_relative 
+
+# require_all 'lib'
